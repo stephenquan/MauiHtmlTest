f0829927232a2e7581bf566f16ff1ea70847148a
Stephen Quan <squan@esri.com>
2025-02-18 21:19:52 +1100

    Improve the font spacing and improve the FormattedStringBuilder

--- MauiHtmlTest/FormattedStringBuilder.cs
+++ MauiHtmlTest/FormattedStringBuilder.cs
@@ -34,45 +34,7 @@ public class FormattedStringBuilder
     {
         if (node.NodeType == HtmlNodeType.Text)
         {
-            Span span = new Span
-            {
-                Text = node.InnerText,
-                TextColor = Colors.Black,
-                TextDecorations = TextDecorations.None,
-                FontAttributes = FontAttributes.None,
-                FontSize = 14,
-            };
-            List<HtmlNode> parentNodes = new List<HtmlNode>();
-            HtmlNode parentNode = node.ParentNode;
-            while (parentNode != null)
-            {
-                parentNodes.Insert(0, parentNode);
-                parentNode = parentNode.ParentNode;
-            }
-            foreach (HtmlNode _parentNode in parentNodes)
-            {
-                switch (_parentNode.Name)
-                {
-                    case "sub":
-                        span.FontFamily = "MyFontSub";
-                        span.FontSize = 10;
-                        break;
-                    case "sup":
-                        span.FontFamily = "MyFontSup";
-                        span.FontSize = 10;
-                        break;
-                    case "b":
-                        span.FontAttributes = FontAttributes.Bold;
-                        break;
-                    case "i":
-                        span.FontAttributes = FontAttributes.Italic;
-                        break;
-                    case "u":
-                        span.TextDecorations = TextDecorations.Underline;
-                        break;
-                }
-            }
-            FormattedString.Spans.Add(span);
+            FormattedString.Spans.Add(NewTextNode(node));
         }
 
         foreach (var child in node.ChildNodes)
@@ -80,5 +42,111 @@ public class FormattedStringBuilder
             AddHtmlNode(child);
         }
     }
+
+    /// <summary>
+    /// Applies a text node to a span.
+    /// </summary>
+    /// <param name="node"></param>
+    public static Span NewTextNode(HtmlNode node)
+    {
+        Span span = new Span
+        {
+            Text = HtmlEntity.DeEntitize(node.InnerText),
+            TextColor = Colors.Black,
+            TextDecorations = TextDecorations.None,
+            FontAttributes = FontAttributes.None,
+            FontSize = 14
+        };
+
+        List<HtmlNode> parentNodes = new List<HtmlNode>();
+        HtmlNode parentNode = node.ParentNode;
+        while (parentNode != null)
+        {
+            parentNodes.Insert(0, parentNode);
+            parentNode = parentNode.ParentNode;
+        }
+
+        foreach (HtmlNode _parentNode in parentNodes)
+        {
+            switch (_parentNode.Name.ToLower())
+            {
+                case "sub":
+                    span.FontFamily = "MyFontSub";
+                    span.FontSize = 10;
+                    break;
+                case "sup":
+                    span.FontFamily = "MyFontSup";
+                    span.FontSize = 10;
+                    break;
+                case "b":
+                    span.FontAttributes = FontAttributes.Bold;
+                    break;
+                case "i":
+                    span.FontAttributes = FontAttributes.Italic;
+                    break;
+                case "u":
+                    span.TextDecorations = TextDecorations.Underline;
+                    break;
+                case "font":
+                    ApplyFont(span, _parentNode);
+                    break;
+            }
+            ApplyStyle(span, _parentNode);
+        }
+
+        return span;
+    }
+
+    /// <summary>
+    /// Applies HTML font attributes to a span.
+    /// </summary>
+    /// <param name="span"></param>
+    /// <param name="fontNode"></param>
+    public static void ApplyFont(Span span, HtmlNode fontNode)
+    {
+        ApplyFontColor(span, fontNode.GetAttributeValue("color", string.Empty));
+    }
+
+    /// <summary>
+    /// Applies HTML font color to a span.
+    /// </summary>
+    /// <param name="span"></param>
+    /// <param name="color"></param>
+    public static void ApplyFontColor(Span span, string color)
+    {
+        if (!string.IsNullOrEmpty(color) && Color.TryParse(color, out Color colorValue))
+        {
+            span.TextColor = colorValue;
+        }
+    }
+
+    /// <summary>
+    /// Applies HTML style to a span.
+    /// </summary>
+    /// <param name="span"></param>
+    /// <param name="node"></param>
+    public static void ApplyStyle(Span span, HtmlNode node)
+    {
+        string style = node.GetAttributeValue("style", string.Empty);
+        if (string.IsNullOrEmpty(style))
+        {
+            return;
+        }
+
+        foreach (string stylePair in style.Split(';'))
+        {
+            string[] styleParts = stylePair.Split(':');
+            if (styleParts.Length == 2)
+            {
+                switch (styleParts[0].Trim().ToLower())
+                {
+                    case "color":
+                        ApplyFontColor(span, styleParts[1].Trim());
+                        break;
+                }
+            }
+        }
+    }
+
 }
 
--- MauiHtmlTest/MainPage.xaml.cs
+++ MauiHtmlTest/MainPage.xaml.cs
@@ -2,11 +2,10 @@
 
 public partial class MainPage : ContentPage
 {
-    public string HtmlText { get; } = "<sup>0123456789</sup>0123456789<sub>0123456789</sub>";
 
     int count = 0;
 
-    public FormattedString FormattedString { get; } = new FormattedString()
+    public FormattedString NumberTest { get; } = new FormattedString()
     {
         Spans =
         {
@@ -15,6 +14,27 @@ public partial class MainPage : ContentPage
             new Span { Text = "0123456789", FontFamily = "MyFontSub", FontSize = 10, },
         }
     };
+
+    public FormattedString FormulaTest { get; } = new FormattedString()
+    {
+        Spans =
+        {
+            new Span { Text = "1", FontFamily = "MyFontSup", FontSize = 10, },
+            new Span { Text = "Almost every developer's favorite molecule is C", FontSize = 14, },
+            new Span { Text = "8", FontFamily = "MyFontSub", FontSize = 10, },
+            new Span { Text = "H", FontSize = 14, },
+            new Span { Text = "10", FontFamily = "MyFontSub", FontSize = 10, },
+            new Span { Text = "N", FontSize = 14, },
+            new Span { Text = "4", FontFamily = "MyFontSub", FontSize = 10, },
+            new Span { Text = "O", FontSize = 14, },
+            new Span { Text = "2", FontFamily = "MyFontSub", FontSize = 10, },
+            new Span { Text = ", also known as ", FontSize = 14 },
+            new Span { Text = "\"caffeine.\"", TextColor = Colors.Red, FontSize = 14, },
+        }
+    };
+
+    public string NumberHtml { get; } = "<sup>0123456789</sup>0123456789<sub>0123456789</sub>";
+    public string FormulaHtml { get; } = """<sup>1</sup>Almost every developer's favorite molecule is C<sub>8</sub>H<sub>10</sub>N<sub>4</sub>O<sub>2</sub>, also known as <span style="color:red">"caffeine."</span> """;
 
     public MainPage()
     {
