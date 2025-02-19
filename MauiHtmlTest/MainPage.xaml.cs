6f6433599136838cc7c0d2b52fd105385f421d50
Stephen Quan <squan@esri.com>
2025-02-19 11:23:58 +1100

    Streamline HTML test removing numbers.

--- MauiHtmlTest/MainPage.xaml.cs
+++ MauiHtmlTest/MainPage.xaml.cs
@@ -5,22 +5,14 @@ public partial class MainPage : ContentPage
 
     int count = 0;
 
-    public FormattedString NumberTest { get; } = new FormattedString()
-    {
-        Spans =
-        {
-            new Span { Text = "0123456789", FontFamily = "MyFontSup", FontSize = 10, },
-            new Span { Text = "0123456789", FontSize = 14, },
-            new Span { Text = "0123456789", FontFamily = "MyFontSub", FontSize = 10, },
-        }
-    };
-
     public FormattedString FormulaTest { get; } = new FormattedString()
     {
         Spans =
         {
             new Span { Text = "1", FontFamily = "MyFontSup", FontSize = 10, },
-            new Span { Text = "Almost every developer's favorite molecule is C", FontSize = 14, },
+            new Span { Text = "Almost every ", FontSize = 14, },
+            new Span { Text = "developer's", FontSize = 14, FontAttributes = FontAttributes.Bold | FontAttributes.Italic, TextColor = Colors.Red },
+            new Span { Text = " favorite molecule is C", FontSize = 14, },
             new Span { Text = "8", FontFamily = "MyFontSub", FontSize = 10, },
             new Span { Text = "H", FontSize = 14, },
             new Span { Text = "10", FontFamily = "MyFontSub", FontSize = 10, },
@@ -29,12 +21,29 @@ public partial class MainPage : ContentPage
             new Span { Text = "O", FontSize = 14, },
             new Span { Text = "2", FontFamily = "MyFontSub", FontSize = 10, },
             new Span { Text = ", also known as ", FontSize = 14 },
-            new Span { Text = "\"caffeine.\"", TextColor = Colors.Red, FontSize = 14, },
+            new Span { Text = "\"caffeine.\"", FontSize = 14, TextColor = Colors.Blue, TextDecorations = TextDecorations.Underline },
         }
     };
 
-    public string NumberHtml { get; } = "<sup>0123456789</sup>0123456789<sub>0123456789</sub>";
-    public string FormulaHtml { get; } = """<sup>1</sup>Almost every developer's favorite molecule is C<sub>8</sub>H<sub>10</sub>N<sub>4</sub>O<sub>2</sub>, also known as <span style="color:red">"caffeine."</span> """;
+    private string demoHtml = @"
+<p>
+  <sup>1</sup>Almost every <b><i><font color=""red"">developer's</font></i></b> favorite molecule is C<sub>8</sub>H<sub>10</sub>N<sub>4</sub>O<sub>2</sub>, also known as
+  <a href=""https://developer.mozilla.org/en-US/docs/Web/HTML/Element/sub"">""caffeine.""</a>
+</p>
+";
+
+    /// <summary>
+    /// Gets or sets a HTML demo string.
+    /// </summary>
+    public string DemoHtml
+    {
+        get => demoHtml;
+        set
+        {
+            demoHtml = value;
+            OnPropertyChanged(nameof(DemoHtml));
+        }
+    }
 
     public MainPage()
     {
