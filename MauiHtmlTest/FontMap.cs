70f027768f14cf965d89241d5f2f2c0bf5db74e2
Stephen Quan <squan@esri.com>
2025-03-13 10:23:31 +1100

    Implement FontMap

---
+++ MauiHtmlTest/FontMap.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiHtmlTest;
+
+public static class FontMap
+{
+    public static Dictionary<Tuple<string, FontAttributes>, string> FontMapDictionary = new();
+
+    public static void Register(string fontFamily, FontAttributes fontAttributes, string newFontFamily)
+    {
+        FontMapDictionary.Add(new Tuple<string, FontAttributes>(fontFamily, fontAttributes), newFontFamily);
+    }
+
+    public static bool Apply(ref string fontFamily, ref FontAttributes fontAttributes)
+    {
+        if (FontMapDictionary.TryGetValue(new Tuple<string, FontAttributes>(fontFamily, fontAttributes), out var newFontFamily))
+        {
+            fontFamily = newFontFamily;
+            fontAttributes = FontAttributes.None;
+            return true;
+        }
+
+        return false;
+    }
+}
--- MauiHtmlTest/MauiProgram.cs
+++ MauiHtmlTest/MauiProgram.cs
@@ -13,6 +13,10 @@ namespace MauiHtmlTest
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("NotoSans-Regular.ttf", "NotoSansRegular");
+                    fonts.AddFont("NotoSans-Bold.ttf", "NotoSansBold");
+                    fonts.AddFont("NotoSans-BoldItalic.ttf", "NotoSansBoldItalic");
+                    fonts.AddFont("NotoSans-Italic.ttf", "NotoSansItalic");
                     fonts.AddFont("myfont-sub.ttf", "MyFontSub");
                     fonts.AddFont("myfont-sup.ttf", "MyFontSup");
                 });
@@ -20,6 +24,10 @@ namespace MauiHtmlTest
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
+
+            FontMap.Register("NotoSansRegular", FontAttributes.Bold, "NotoSansBold");
+            FontMap.Register("NotoSansRegular", FontAttributes.Italic, "NotoSansItalic");
+            FontMap.Register("NotoSansRegular", FontAttributes.Bold | FontAttributes.Italic, "NotoSansBoldItalic");
 
             return builder.Build();
         }
